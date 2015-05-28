973f01c5d265e174de60f7906a41b9f41942c675
Maciej Kucharski <maciek.zir@gmail.com>
2015-05-28 11:19:15 +0200

    [master] parsing the id attribute bug

--- task-5.1/models/book.es6.js
+++ task-5.1/models/book.es6.js
@@ -1,6 +1,13 @@
+/*
+ *   The reason to use the `idAttribute` and then persist the `trackId` property while parsing
+ *   is an undocumented bug in Backbone (appeared in 1.1.0, not fixed in current version[1.2.0]),
+ *   referenced in #3533, #3638, #3529
+ * */
 var Book = Backbone.Model.extend({
+    idAttribute: 'trackId',
     parse: function(attrs) {
         return {
+            trackId: attrs.trackId,
             id: attrs.trackId,
             title: attrs.trackName,
             author: attrs.artistName,
